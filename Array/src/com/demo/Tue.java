0e4d5fa75b2578a8dc7d4ed1e5f83be8d1754a96
Sohel <patvekarsuhel@gmail.com>
2023-09-11 09:08:33 +0530

    First commit of Array Practical

---
+++ Array/src/com/demo/Tue.java
@@ -0,0 +1,88 @@
+package com.demo;
+
+public class Tue {
+	public static void main(String[] args) {
+//		int[] a= {10,20,30,40,50};
+		
+//		Print by using for loop..
+	/*	for(int i=0; i<a.length; i++)
+		{
+			System.out.println(a[i]);
+		}*/
+		
+//		Print by using for each loop..
+	/*	for (int i : a) {
+			System.out.println(i);
+		}*/
+		
+		System.out.println("---------------------------------------------");
+		
+//		Declaration int[] + Creation + Initilization
+//		int[] a= {102,103,104,105,106};
+		
+//		By using for loop..
+	/*	for(int i=0; i<a.length; i++) {
+			System.out.println(a[i]);
+		}*/
+		
+//		By using for each loop..
+	/*	for (int i : a) {
+			System.out.println(i);
+		}*/
+		
+		System.out.println("---------------------------------------------");
+
+	/*	int[] a=new int[3];
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;*/
+		
+//		Print by using for each loop..
+	/*	for(int i=0; i<a.length; i++) {
+			System.out.println(a[i]);
+		}*/
+		
+//		Print by using for each loop..
+	/*	for(int i:a) {
+			System.out.println(i);
+		}*/
+		
+		System.out.println("---------------------------------------------");
+
+//		int[] s= {10,20,30,40,50};
+		
+//		Print By using for loop..
+	/*	for(int i=0;i<s.length; i++) {
+		System.out.println(s[i]);
+		}*/
+		
+//		Print By using foreach loop..
+	/*	for(int i:s) {
+			System.out.println(i);
+		}*/
+		
+		System.out.println("---------------------------------------------");
+
+	/*	int[] d;
+		d=new int[5];
+		d[0]=10;
+		d[1]=20;
+		d[2]=30;
+		d[3]=40;
+		d[4]=50;*/
+		
+//		Print by using for loop..
+	/*	for(int i = 0; i < d.length; i++ ) {
+			System.out.println(d[i]);
+		}*/
+		
+//		Print By using for each loop..
+	/*	for (int j : d) {
+			System.out.println(j);
+		}*/
+		
+
+		
+		
+}
+}
